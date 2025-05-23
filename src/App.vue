1ee54e50cfbe1a156adad5112530d7d8904146b2
切符 <KIPPUDESU@OUTLOOK.com>
2025-05-23 22:25:33 +0800

    Chronicle Project Initial Creation
    苹果产品介绍项目，目前感觉有点难度
    今天没什么时间，先敷衍一个Card上去
    放在这里，慢慢完善一下

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+  class="
+  min-h-screen
+  bg-gradient-to-r
+  from-pink-500 to-white
+  "
+  >
+    <MiniCard />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MiniCard from './components/MiniCard.vue';
+
+
+</script>
+
+<style>
+@import "tailwindcss";
+</style>
---
+++ src/components/MiniCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div
+    class="
+    absolute
+    h-80
+    w-60
+    top-1/4
+    inset-x-0
+    mx-auto
+    rounded-2xl
+    drop-shadow-2xl
+    bg-gradient-to-r
+    from-white to-gray-100
+    "
+    >
+    <div
+    class="
+    absolute
+    top-5
+    "
+    >
+      <img src="../images/MacBookAirM2.png" alt="">
+    </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>

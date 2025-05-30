b54eb87e51b39a483958e1c5bc05176277c4c8f1
切符 <KIPPUDESU@OUTLOOK.com>
2025-05-30 20:59:54 +0800

    Updated the homepage layout
    仿照苹果官网的风格做好了一组卡片样式，后期做响应式绑定

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,84 @@
+<template>
+<div class="flex">
+    <div 
+    class="flex
+    bg-white m-1 overflow-x-auto w-max h-150
+    "
+    >
+    <div
+    class="flex relative
+    bg-gradient-to-b from-blue-100 to-white m-1 w-100 h-125
+    rounded-2xl shadow-lg overflow-hidden
+    "
+    >
+    <div class="
+    absolute flex flex-col gap-1.5 top-1/10 left-1/12
+    ">
+        <h1 class="font-bold text-[30px] text-center z-10">MacBook Air</h1>
+        <p class="font-bold text-[14px]">身轻身手快。</p>
+    </div>
+    <img src="../images/macbookbig.png" alt="">
+    </div>
+    <div
+    class="flex relative
+    bg-gradient-to-b from-blue-100 to-white m-1 w-100 h-125
+    rounded-2xl shadow-lg overflow-hidden
+    "
+    >
+    <div class="
+    absolute flex flex-col gap-1.5 top-1/10 left-1/12
+    ">
+        <h1 class="font-bold text-[30px] text-center z-10">MacBook Air</h1>
+        <p class="font-bold text-[14px]">身轻身手快。</p>
+    </div>
+    <img src="../images/macbookbig.png" alt="">
+    </div>
+    <div
+    class="flex relative
+    bg-gradient-to-b from-blue-100 to-white m-1 w-100 h-125
+    rounded-2xl shadow-lg overflow-hidden
+    "
+    >
+    <div class="
+    absolute flex flex-col gap-1.5 top-1/10 left-1/12
+    ">
+        <h1 class="font-bold text-[30px] text-center z-10">MacBook Air</h1>
+        <p class="font-bold text-[14px]">身轻身手快。</p>
+    </div>
+    <img src="../images/macbookbig.png" alt="">
+    </div>
+    <div
+    class="flex relative
+    bg-gradient-to-b from-blue-100 to-white m-1 w-100 h-125
+    rounded-2xl shadow-lg overflow-hidden
+    "
+    >
+    <div class="
+    absolute flex flex-col gap-1.5 top-1/10 left-1/12
+    ">
+        <h1 class="font-bold text-[30px] text-center z-10">MacBook Air</h1>
+        <p class="font-bold text-[14px]">身轻身手快。</p>
+    </div>
+    <img src="../images/macbookbig.png" alt="">
+    </div>
+    <div
+    class="flex relative
+    bg-gradient-to-b from-blue-100 to-white m-1 w-100 h-125
+    rounded-2xl shadow-lg overflow-hidden
+    "
+    >
+    <div class="
+    absolute flex flex-col gap-1.5 top-1/10 left-1/12
+    ">
+        <h1 class="font-bold text-[30px] text-center z-10">MacBook Air</h1>
+        <p class="font-bold text-[14px]">身轻身手快。</p>
+    </div>
+    <img src="../images/macbookbig.png" alt="">
+    </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,25 @@
 <template>
+<div 
+class="flex flex-col gap-10 h-max w-max"
+>
 <div
 class="
-flex h-screen justify-center items-center  text-8xl text-center font-bold
+flex h-screen w-screen justify-center items-center text-8xl text-center font-bold
+bg-gradient-to-br from-gray-100 to-gray-400
 "
->这里是主页</div>
+>这里是主页上方</div>
+<div
+class="flex h-350 "
+>
+<div>
+<HomeCard />
+</div>
+</div>
+</div>
 </template>
 
 <script setup="ts">
-
+import HomeCard from '../components/HomeCard.vue';
 </script>
 
 <style>
